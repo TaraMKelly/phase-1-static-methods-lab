117d717fafc8009a27c414b61407726c94a49c83
TaraMKelly <tara.kelly16@gmail.com>
2021-07-15 20:51:35 -0400

    commit tree 7e442ffea5a036a31272f166e47fab3e2eb83651

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1)
+  }
+  static sanitize(str) {
+    return str.replace(/[^\w\s\'\-]/g, "") //regex syntax!!
+  }
+  static titleize(str) {
+    let arr = [];
+    let exludeWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    arr = str.split(' ');
+    arr[0] = arr[0].charAt(0).toUpperCase() + arr[0].slice(1);
+    return arr.map(word => {
+      return exludeWords.includes(word) ? [word] : word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(' ')
+  }
 }
